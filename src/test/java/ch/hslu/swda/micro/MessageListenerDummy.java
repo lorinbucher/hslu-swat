a5ad2b1f89b9bf8b3deb2566dcdd0f475de5bb6a
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2023-12-10 00:46:48 +0100

    test: add message listener dummy used for testing

---
+++ src/test/java/ch/hslu/swda/micro/MessageListenerDummy.java
@@ -0,0 +1,25 @@
+package ch.hslu.swda.micro;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * Dummy implementation of the message listener used for testing.
+ */
+public class MessageListenerDummy implements MessageListener {
+
+    private final Map<String, Consumer<String>> callbacks = new HashMap<>();
+
+    @Override
+    public void receiveMessages(final String route, final Consumer<String> callback) {
+        callbacks.put(route, callback);
+    }
+
+    public void mockMessage(final String route, final String message) {
+        Consumer<String> callback = callbacks.get(route);
+        if (callback != null) {
+            callback.accept(message);
+        }
+    }
+}
