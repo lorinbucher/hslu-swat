a8c2140def7e2b7d87631f61f7684fd8c0a6cfd0
Lorin Bucher <lorin.bucher@stud.hslu.ch>
2023-12-10 11:15:43 +0100

    test: add unit tests for article message processor

--- src/test/java/ch/hslu/swda/micro/ArticleMessageProcessorTest.java
+++ src/test/java/ch/hslu/swda/micro/ArticleMessageProcessorTest.java
@@ -1,49 +1,74 @@
 package ch.hslu.swda.micro;
 
-import ch.hslu.swda.business.Deliveries;
-import ch.hslu.swda.business.DeliveriesMemory;
 import ch.hslu.swda.business.ProductCatalog;
 import ch.hslu.swda.business.ProductCatalogMemory;
-import ch.hslu.swda.entities.*;
+import ch.hslu.swda.dto.OrderDTO;
+import ch.hslu.swda.entities.Article;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Test cases for the article message processor.
  */
 class ArticleMessageProcessorTest {
 
-//    private ProductCatalog productCatalog;
-//
-//    @BeforeEach
-//    void initializeEnv() {
-//        productCatalog = new ProductCatalogMemory();
-//        productCatalog.create(1, new Article(100001L, "Article 1", new BigDecimal("5.25"), 1, 1));
-//        productCatalog.create(1, new Article(100002L, "Article 2", new BigDecimal("9.95"), 2, 2));
-//    }
-//
-//    @Test
-//    void testInvalidMessageReceived() {
-//        ArticleMessageProcessor processor = new ArticleMessageProcessor(productCatalog);
-//        String message = "{}";
-//        assertThatThrownBy(() -> processor.process(message))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("Cannot parse received article message");
-//    }
-//
-//    @Test
-//    void testValidMessageReceived() {
-//        ArticleMessageProcessor processor = new ArticleMessageProcessor(productCatalog);
-//        String message = "{\"branchId\":1,\"orderNumber\":5,\"articles\":[100001,100005]}";
-//        String response = processor.process(message);
-//        assertThat(response).isEqualTo("{\"branchId\":1,\"orderNumber\":5," +
-//                "\"articles\":[{\"articleId\":100001,\"name\":\"Article 1\",\"price\":5.25}]," +
-//                "\"error\":[\"article 100005 not found in catalog\"]}");
-//    }
+    private MessageListenerDummy messageListener;
+    private MessagePublisherDummy<OrderDTO> messagePublisher;
+
+    @BeforeEach
+    void initializeEnv() {
+        ProductCatalog productCatalog = new ProductCatalogMemory();
+        productCatalog.create(1, new Article(100001L, "Article 1", new BigDecimal("5.25"), 1, 1));
+        productCatalog.create(1, new Article(100002L, "Article 2", new BigDecimal("9.95"), 2, 2));
+
+        messageListener = new MessageListenerDummy();
+        messagePublisher = new MessagePublisherDummy<>();
+        ArticleMessageProcessor articleMessageProcessor = new ArticleMessageProcessor(messageListener, messagePublisher, productCatalog);
+        articleMessageProcessor.run();
+    }
+
+    @Test
+    void testInvalidMessageReceived() throws InterruptedException {
+        String message = "{}";
+        messageListener.mockMessage(Routes.ARTICLE_GET, message);
+        TimeUnit.MILLISECONDS.sleep(100);
+        String response = messagePublisher.getMessage(Routes.ARTICLE_RETURN);
+        assertThat(response).isNull();
+    }
+
+    @Test
+    void testValidMessageReceivedError() throws InterruptedException {
+        String message = "{\"branchId\":1,\"orderNumber\":5,\"articles\":[100005]}";
+        messageListener.mockMessage(Routes.ARTICLE_GET, message);
+        TimeUnit.MILLISECONDS.sleep(100);
+        String response = messagePublisher.getMessage(Routes.ARTICLE_RETURN);
+        assertThat(response).isEqualTo("{\"branchId\":1,\"orderNumber\":5," +
+                "\"articles\":[],\"error\":[\"article 100005 not found in catalog\"]}");
+    }
+
+    @Test
+    void testValidMessageReceivedNoError() throws InterruptedException {
+        String message = "{\"branchId\":1,\"orderNumber\":5,\"articles\":[100001]}";
+        messageListener.mockMessage(Routes.ARTICLE_GET, message);
+        TimeUnit.MILLISECONDS.sleep(100);
+        String response = messagePublisher.getMessage(Routes.ARTICLE_RETURN);
+        assertThat(response).isEqualTo("{\"branchId\":1,\"orderNumber\":5," +
+                "\"articles\":[{\"articleId\":100001,\"name\":\"Article 1\",\"price\":5.25}],\"error\":[]}");
+    }
+
+    @Test
+    void testValidMessageReceivedMixed() throws InterruptedException {
+        String message = "{\"branchId\":1,\"orderNumber\":5,\"articles\":[100001,100005]}";
+        messageListener.mockMessage(Routes.ARTICLE_GET, message);
+        TimeUnit.MILLISECONDS.sleep(100);
+        String response = messagePublisher.getMessage(Routes.ARTICLE_RETURN);
+        assertThat(response).isEqualTo("{\"branchId\":1,\"orderNumber\":5," +
+                "\"articles\":[{\"articleId\":100001,\"name\":\"Article 1\",\"price\":5.25}]," +
+                "\"error\":[\"article 100005 not found in catalog\"]}");
+    }
 }
